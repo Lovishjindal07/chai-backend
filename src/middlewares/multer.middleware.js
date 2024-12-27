9c9768ea2c85971c023367b6ee3d294a37cb3965
Lovishjindal07 <lovishjindala72@gmail.com>
2024-12-27 14:45:05 +0530

    all setup configured

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+
+// Here we are using disk storage
+
+const storage = multer.diskStorage({ 
+    destination: function (req, file, cb) {
+        cb(null,"./public/temp")
+    },
+    filename: function(req,file,cb) {
+        cb(null,file.originalname)
+    }
+});
+
+export const uplaod = multer({
+    storage: storage
+})
