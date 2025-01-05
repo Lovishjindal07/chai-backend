114867bc90de80313ca820b8c7830b3a3243b74a
Lovishjindal07 <lovishjindala72@gmail.com>
2025-01-05 15:34:02 +0530

    add refreshAccessTokne endpoint

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,7 +1,10 @@
+
+import CircularJSON from 'circular-json';
+
 class ApiResponse {
     constructor(statusCode, data, message="Success"){
         this.statusCode = statusCode,
-        this.data= data,
+        this.data= CircularJSON.stringify(data),
         this.message = message,
         this.success = statusCode < 400
     }
